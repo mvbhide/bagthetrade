eb8b7af64ba06840ac192085426c2db1b9e95f40
Milind Bhide <mvbhide@gmail.com>
2017-12-19 11:28:32 +0530

    ticker functionalities

--- express/commodity_quick_trade.js
+++ express/commodity_quick_trade.js
@@ -2,34 +2,35 @@ var KiteConnect = require("kiteconnect").KiteConnect;
 var KiteTicker = require("kiteconnect").KiteTicker;
 var config = require('./config')
 
-var actk = 'yraq5icbqmiyarnff6hlr7rx94kmux2p';
+var actk = '92nycxdt4hxdo74bv9djk8sgsorbxqyc';
 var kc = new KiteConnect(config.API_KEY, {access_token: actk});
 
 var transaction_type = 'BUY';
-var stop_loss = '3782';
-var price = '3796';
-var target = '3814'
+var stop_loss = '3715';
+var price = '3732';
+var target = '3335'
+var tradingsymbol = 'CRUDEOIL17DECFUT'
 
 var order_id = '';
 let payload = {
-	tradingsymbol: 'CRUDEOILM17DECFUT',
+	tradingsymbol: tradingsymbol,
 	exchange: 'MCX',
 	segment: 'commodity',
 	transaction_type: transaction_type,
 	trigger_price: stop_loss,
-	quantity: '3',
+	quantity: '1',
 	order_type: 'MARKET',
 	product: 'MIS',
 	validity: 'DAY'
 }
-
+var order_executed = false;
 var ticker = new KiteTicker('2ii3pn7061sv4cmf', 'RP6292', '4d3d5784e80affaa3c15b9e37fd2f690');
 ticker.connect();
 
 ticker.on("connect", function() {
 	console.log("ticker connected");
-	var order_executed = false;
-	ticker.setMode(ticker.modeFull, [53480711]);
+	
+	ticker.setMode(ticker.modeFull, [53480455]);
 	ticker.on("tick", function(ticks) {
 		let ltp = ticks[0].LastTradedPrice;
 		let topAsk = ticks[0].Depth.buy[0].Price;
@@ -37,7 +38,23 @@ ticker.on("connect", function() {
 		
 		console.log(topAsk + "     " + topBid);
 
-		if(topBid == price && order_executed == false) {
+		if(transaction_type == 'BUY') {
+			if(topBid <= price && order_executed == false) {
+				placeorder()
+			}
+			if(topAsk >= target && order_executed == true && order_id != '') {
+				exitorder();
+			}
+		} else if(transaction_type == 'SELL') {
+			if(topAsk >= price && order_executed == false) {
+				placeorder()
+			}
+			if(topBid <= target && order_executed == true && order_id != '') {
+				exitorder();
+			}
+		}
+
+		function placeorder() {
 			kc.orderPlace(payload, 'co')
 			.then(function(res) {
 				console.log('Order Executed');
@@ -56,7 +73,8 @@ ticker.on("connect", function() {
 				order_executed = true;
 			})	
 		}
-		if(topAsk >= target && order_executed == true && order_id != '') {
+
+		function exitorder() {
 			console.log(order_id);
 			kc.orderCancel(order_id, 'co')
 			.then(function(res) {
--- express/oms.js
+++ express/oms.js
@@ -1,6 +1,7 @@
 var objFactory = require('./object-factory/fp');
 var KiteConnect = require("kiteconnect").KiteConnect;
 var _ = require('lodash/fp');
+var db = require('./db');
 var config = require('./config')
 
 
@@ -11,18 +12,24 @@ var oms = {};
 
 oms.placeOrder = function(objOrder) {
 
-	var orderParams = _.extend(objFactory.get('placeOrder'), objOrder);
-	return kc.orderPlace(orderParams)
-	.then(function(res) {
-		if(res && res.data) {
-			if(res.status === "success") {
-				return res.data.order_id;
+	db.getAccessToken(config.API_KEY)
+	.then(function(response) {
+		var actk = response.access_token;
+		var kc = new KiteConnect(config.API_KEY, {access_token: actk});
+
+		var orderParams = _.extend(objFactory.get('placeOrder'), objOrder);
+		return kc.orderPlace(orderParams)
+		.then(function(res) {
+			if(res && res.data) {
+				if(res.status === "success") {
+					return res.data.order_id;
+				}
 			}
-		}
-	})
-	.catch(function(err) {
-		console.log(err)
-	})
+		})
+		.catch(function(err) {
+			console.log(err)
+		})
+	});
 }
 
 module.exports = oms
