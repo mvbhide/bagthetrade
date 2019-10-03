083d2b575b4dbc2f837950a1bf8c426afee0363b
Milind <mvbhide@gmail.com>
2019-10-03 11:40:02 +0530

    updates

--- express/routes/upstox.js
+++ express/routes/upstox.js
@@ -16,9 +16,13 @@ var _ = require('lodash');
 var upstox = new Upstox(config.API_KEY);
 
 var schedule = require('node-schedule');
- 
-var j = schedule.scheduleJob('2 */5 * * * *', function(){
-  console.log('The answer to life, the universe, and everything!');
+
+var j = schedule.scheduleJob('2 */15 * * * *', function(){
+	let hour = new Date().getHours();
+	if(hour > 8 && hour <16) {
+		getdata();
+	}
+
 });
 
 router.get("/", function(req, res, next) {
@@ -57,6 +61,79 @@ router.get("/auth", function(req, res, next) {
 	
 
 router.get("/getdata", function(req, res, next) {
+	var data = getdata(res);
+})
+
+router.get("/test", function(req, res, next) {
+	var d = getPreviousDay();
+	res.end(getFormattedDate(d));
+})
+
+router.get("/getdata/:id", function(req, res, next) {
+	var symbol = req.params.id;
+	fs.readFile("../token.txt", function(err, data) {
+		upstox.setToken(data);
+		callApi(symbol)
+		.then(function(response) {
+			response.data.reverse();
+			var result = scanner(response)
+			res.end(JSON.stringify(response))
+		})
+		.catch(function(err) {
+			res.end("Error : " + JSON.stringify(err));
+		})
+	})
+})
+
+var callApi = function(symbol) {
+	return upstox.getOHLC({
+		"exchange": "NSE_EQ",
+		"symbol": symbol,
+		"format": "json",
+		"interval": "15MINUTE",
+		"start_date": previousDay,
+		"end_date": today
+	})
+	.then(function (response) {
+		console.log(symbol + " API successful")
+		response.symbol = symbol
+		return response;
+	})
+	.catch(function (error) {
+		//response.end("Error : " + JSON.stringify(err));
+		//throw error;
+	});
+}
+
+var getFormattedDate = (d) => {
+	var dd = d.getDate(); 
+	var mm = d.getMonth() + 1; 
+	var yyyy = d.getFullYear(); 
+	dd = dd < 10 ? '0' + dd : dd 
+	mm =  (mm < 10) ?'0' + mm : mm; 
+	return dd + '-' + mm + '-' + yyyy; 
+}
+
+var getPreviousDay = function() {
+	var d = new Date(2019,9,4);
+	console.log(d)
+	var arrHoliays = [
+		"02-10-2019",
+		"08-10-2019",
+		"28-10-2019",
+		"12-11-2019",
+		"25-12-2019"
+	]
+	do {
+		d.setDate(d.getDate() - 1);
+	} while((d.getDay() == 0 || d.getDay() == 6) && !arrHoliays.includes(getFormattedDate(d)))
+	return d;
+}
+
+var today = "03-10-2019"; //getFormattedDate(new Date());
+var previousDay = "01-10-2019";//getFormattedDate(getPreviousDay());
+console.log(today, previousDay)
+var getdata = function(out = "") {
 	fs.readFile("../token.txt", function(err, data) {
 		upstox.setToken(data);		
 		var count = 0;
@@ -64,14 +141,9 @@ router.get("/getdata", function(req, res, next) {
 		arrCalls = []
 		arrObjResponse = [];
 		var loop = setInterval(function() {
-			console.log('timeout called')
 			for(i = 0; i < 10; count++, i++) {
 				callApi(stocks[count])
 				.then(function(priceData) {
-					/* if(priceData == undefined || priceData.data ==  undefined || priceData.data.length == 0) {
-						console.log("Data issue for " + priceData.symbol);
-						return;
-					} */
 					resolvedCount++;
 					if(priceData && priceData.data) {
 						priceData.data.reverse();
@@ -80,7 +152,6 @@ router.get("/getdata", function(req, res, next) {
 						obj.result = scanner(priceData);
 						arrObjResponse.push(obj);
 					}
-					
 				})
 			}
 			if(count >= (stocks.length - 1)) {
@@ -96,61 +167,23 @@ router.get("/getdata", function(req, res, next) {
 						dataToSend.push(element);
 					}
 				});
-				//sendmail(dataToSend);
 				var html = generateHTML(dataToSend);
-				console.log(html)
-				sendmail(html);
-				res.end(html);
+				if(out !== "") {
+					out.end(html);
+				} else {
+					sendmail(html);
+				}
+				return dataToSend;
 			}
 		},1000)
 	})		
-})
-
-router.get("/test", function(req, res, next) {
-
-})
-
-router.get("/getdata/:id", function(req, res, next) {
-	var symbol = req.params.id;
-	fs.readFile("../token.txt", function(err, data) {
-		upstox.setToken(data);
-		callApi(symbol)
-		.then(function(response) {
-			res.end(JSON.stringify(response))
-		})
-		.catch(function(err) {
-			res.end("Error : " + JSON.stringify(err));
-		})
-	})
-})
-
-var callApi = function(symbol) {
-	var today = new Date().toJSON().slice(0,10).split('-').reverse().join('-');
-		return upstox.getOHLC({
-			"exchange": "NSE_EQ",
-			"symbol": symbol,
-			"format": "json",
-			"interval": "15MINUTE",
-			"start_date": today,
-			"end_date": today
-		})
-		.then(function (response) {
-			response.symbol = symbol
-			return response;
-		})
-		.catch(function (error) {
-			//response.end("Error : " + JSON.stringify(err));
-			//throw error;
-		});
 }
 
 var scanner = function(objData) {
 	var data = objData.data
 	var objCandle = {};
 	var c = []
-	if(data.length < 2) {
-		return;
-	}
+	
 	for(var i = 0; i < 4; i++) {
 		if(typeof data[i] == 'undefined') break;
 		objCandle.date = data[i].timestamp;
@@ -175,23 +208,19 @@ var scanner = function(objData) {
 		c[i] = objCandle;
 		objCandle = {};
 	}
-/* console.log(c[0]);
-console.log((c[0].high-c[0].bodyCenter), (c[0].bodyCenter - c[0].low)/5)
-console.log((c[0].high-c[0].bodyCenter) < (c[0].bodyCenter - c[0].low)/5)
-console.log((c[0].bodyCenter - c[0].low), (c[0].high - c[0].bodyCenter)/5);
-console.log((c[0].bodyCenter - c[0].low)< (c[0].high - c[0].bodyCenter)/5); */
+
 	var avg_vol = 0;
 
 	var result = {};
 	result.trend = "";
 	result.pattern = "";
-	result.candle = {
+	/* result.candle = {
 		...c[0],
 		downtrendReversalCondition: "" + (c[0].high-c[0].bodyCenter) + "," + (c[0].bodyCenter - c[0].low)/5,
 		downtrendReversalResult: (c[0].high-c[0].bodyCenter) < (c[0].bodyCenter - c[0].low)/5,
 		uptrendReversalCondition: "" + (c[0].bodyCenter - c[0].low) + "," + (c[0].high - c[0].bodyCenter)/5,
 		uptrendReversalResult: (c[0].bodyCenter - c[0].low) < (c[0].high - c[0].bodyCenter)/5
-	};
+	}; */
 
 /*	if( ( c2.high < c3.high < c4.high) && ( c2.low > c3.low > c4.low) ) {
 		result.trend = "down";
@@ -201,16 +230,23 @@ console.log((c[0].bodyCenter - c[0].low)< (c[0].high - c[0].bodyCenter)/5); */
 		result.trend = "up";
 	}*/
 
-	if(c[0].body < (0.2 * c[0].candleSize) ) {
+	if( (c[1].candleSize < c[0].candleSize) && (c[0].body < (0.2 * c[0].candleSize)) ) {
 		if( (c[0].high-c[0].bodyCenter) < (c[0].bodyCenter - c[0].low)/5 ) {
-			result.pattern = "Downtrend reversal exhaution Candle";	
+			result.pattern = "&#8593;";
+			result.price = c[0].close;
+			result.threshold = c[0].low + "(" + (c[0].close - c[0].low).toFixed(2) + ")";
 		}
 		if( (c[0].bodyCenter - c[0].low) < (c[0].high - c[0].bodyCenter)/5 )  {
-			result.pattern = "Uptrend reversal exhaution Candle";	
+			result.pattern = "&#8595;";	
+			result.price = c[0].close;
+			result.threshold = c[0].high + "(" + (c[0].high - c[0].close).toFixed(2) + ")";
+		}
+		if( c[0].volume > (c[1].volume + c[2].volume + c[3].volume)) {
+			result.pattern += "Volume surge"
 		}
 	}
 
-	if( (c[0].open > c[1].high) && (c[0].close < c[1].bodyCenter) ) {
+	/* if( (c[0].open > c[1].high) && (c[0].close < c[1].bodyCenter) ) {
 		result.pattern = "Dark cloud cover";
 	}
 
@@ -220,7 +256,7 @@ console.log((c[0].bodyCenter - c[0].low)< (c[0].high - c[0].bodyCenter)/5); */
 
 	if( c[0].color == "Green"  && (c[0].open < c[1].low) && (c[0].close > c[1].high) ) {
 		result.pattern = "Bullish Engulfing candle"
-	}
+	} */
 
 	return result;
 }
@@ -253,29 +289,29 @@ var sendmail = function(body) {
 }
 
 var generateHTML = function(data) {
+
+	var getclass = function(p) {
+		return p == "&#8593;" ? "green" : "red";
+	}
+
 	var tableHtml = "";
 	for(i=0; i< data.length; i++) {
-		tableHtml += "<tr><td>" + data[i].symbol + "</td><td>" + data[i].result.pattern + "</td></tr>";
+		tableHtml += "<tr><td>" + data[i].symbol + "</td><td class='" + getclass(data[i].result.pattern) + "'>" + data[i].result.pattern + "</td><td>" + data[i].result.price + "</td><td>" + data[i].result.threshold + "</tr>";
 	}
-	
+
 	var html = `
 		<html>
 		<head>
 		<style>
-		body  {
-			font-family: 'Calibri';
-			
-		}
-		table {
-			border-collapse: collapse;
-		}
-		table tr td {
-			padding: 5px;
-		}
+			body  {font-family: 'Calibri';}
+			table {border-collapse: collapse;}
+			table tr td {padding: 5px;}
+			.green { color : green; font-weight: bold}
+			.red { color: red; font-weight: bold}
 		</style>
 		</head>
 		<body>
-			<table border="1"><thead><th>Symbol</th><th>Pattern</th></thead> ` + tableHtml +`</table>
+			<table border="1"><thead><th>Symbol</th><th>Pattern</th><th>Price</th><th>Threshold</th></thead> ` + tableHtml +`</table>
 		</body>
 		</html>
 	`
